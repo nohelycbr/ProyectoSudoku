28d1a0e231e807eff1270265992fa4c0e1c83e44
Nohely Cruz <nohelyc21@gmail.com>
2019-09-27 22:08:28 -0500

    Está lista la estructura de main

--- Funciones.py
+++ Funciones.py
@@ -1,45 +1,186 @@
-from colorama import Cursor,init, Fore
+from colorama import Cursor, init, Fore
+
 init()
 
-def crear_tabla_vacia ():
-    return [['*','*','*','*','*','*',1,'*',"*"],
-            ['*',4,7,1,6,'*','*','*','*'],
-            ['*',2,'*',5,3,4,'*','*',7],
-            ['*',6,1,'*','*',7,8,'*','*'],
-            ['*',7,4,'*',9,'*',3,1,'*'],
-            ['*','*',2,6,'*','*',4,7,'*'],
-            [2,'*','*',3,4,5,'*',6,'*'],
-            ['*','*','*','*',7,1,9,5,'*'],
-            ['*','*',3,'*','*','*','*','*','*']]
 
-def solucion ():
-    return [[9,6,3,1,7,4,2,5,8],
-            [1,7,8,3,2,5,6,4,9],
-            [2,5,4,6,8,9,7,3,1],
-            [8,2,1,4,3,7,5,9,6],
-            [4,9,6,8,5,2,3,1,7],
-            [7,3,5,9,6,1,8,2,4],
-            [5,8,9,7,1,3,4,6,2],
-            [3,1,7,2,4,6,9,8,5],
-            [6,4,2,5,9,8,1,7,3]]
+def nivel_facil():
+    f1 = [['*', '*', '*', '*', '*', '*', 1, '*', "*"],
+          ['*', 4, 7, 1, 6, '*', '*', '*', '*'],
+          ['*', 2, '*', 5, 3, 4, '*', '*', 7],
+          ['*', 6, 1, '*', '*', 7, 8, '*', '*'],
+          ['*', 7, 4, '*', 9, '*', 3, 1, '*'],
+          ['*', '*', 2, 6, '*', '*', 4, 7, '*'],
+          [2, '*', '*', 3, 4, 5, '*', 6, '*'],
+          ['*', '*', '*', '*', 7, 1, 9, 5, '*'],
+          ['*', '*', 3, '*', '*', '*', '*', '*', '*']]
 
-def VisualizaTabla(tabla):
-    contador_col = 0
-    contador_fila = 0
-    indices = [0,1,2,3,4,5,6,7,8]
+    f2 = [['*', '*', '*', 4, '*', 3, '*', '*', '*'],
+          ['*', '*', 5, 9, '*', 7, 2, '*', '*'],
+          ['*', 3, '*', '*', 6, '*', '*', 7, '*'],
+          ['*', 9, '*', 7, '*', 8, '*', 2, '*'],
+          ['*', 4, 3, '*', '*', '*', 7, 9, '*'],
+          ['*', 7, '*', 3, '*', 2, '*', 5, '*'],
+          ['*', 1, '*', '*', 5, '*', '*', 3, '*'],
+          ['*', '*', 9, 2, '*', 6, 4, '*', '*'],
+          ['*', '*', '*', 1, '*', 4, '*', '*', '*']]
 
-    print("  |",indices[0],indices[1],indices[2],"|",indices[3],indices[4],indices[5],"|",indices[6],indices[7],indices[8])
-    print("   ----------------------")
-    for fila in tabla:
-        print(indices[contador_col],"|",fila[0],fila[1],fila[2],"|",fila[3],fila[4],fila[5],"|",fila[6],fila[7],fila[8])
-        if(contador_col == 2 or contador_col== 5 or contador_col== 8):
-                print("   ----------------------")
-        contador_col+=1
+    f3 = [['*', '*', 5, '*', '*', '*', 7, '*', '*'],
+          ['*', 8, '*', 1, '*', 5, '*', 2, '*'],
+          [9, '*', '*', 4, 6, 7, '*', '*', 5],
+          ['*', 5, 6, 2, '*', 1, 3, 8, '*'],
+          ['*', '*', 8, '*', '*', '*', 9, '*', '*'],
+          ['*', 9, 2, 5, '*', 8, 6, 7, '*'],
+          [2, '*', '*', 8, 5, 9, '*', '*', 3],
+          ['*', 6, '*', 7, '*', 3, '*', 4, '*'],
+          ['*', '*', 3, '*', '*', '*', 5, '*', '*']]
 
-def VisualizaTabla_2(tabla,noModificables):
+    f4 = [['*', '*', 8, 2, '*', 9, 5, '*', '*'],
+          ['*', '*', 6, 1, '*', 5, 4, '*', '*'],
+          [5, 9, '*', '*', '*', '*', '*', 2, 7],
+          [6, 7, '*', '*', 9, '*', '*', 4, 5],
+          ['*', '*', '*', 3, '*', 4, '*', '*', '*'],
+          [2, 4, '*', '*', 1, '*', '*', 8, 9],
+          [3, 6, '*', '*', '*', '*', '*', 5, 8],
+          ['*', '*', 2, 5, '*', 8, 7, '*', '*'],
+          ['*', '*', 4, 7, '*', 6, 9, '*', '*']]
+
+    f5 = [['*', '*', '*', 4, '*', 5, '*', '*', '*'],
+          ['*', '*', 6, 2, 1, 7, 9, '*', '*'],
+          ['*', 8, 4, 9, '*', 6, 5, 1, '*'],
+          [8, 6, 9, '*', '*', '*', 7, 5, 1],
+          ['*', 7, '*', '*', '*', '*', '*', 4, '*'],
+          [5, 4, 2, '*', '*', '*', 6, 9, 3],
+          ['*', 9, 5, 8, '*', 1, 4, 3, '*'],
+          ['*', '*', 1, 7, 9, 2, 8, '*', '*'],
+          ['*', '*', '*', 5, '*', 3, '*', '*', '*']]
+
+    return [f1, f2, f3, f4, f5]
+
+
+def nivel_inter():
+    i1 = [['*', 6, 9, 2, '*', 3, 4, 5, '*'],
+          [5, '*', 7, '*', '*', '*', 9, '*', 1],
+          [3, '*', '*', '*', '*', '*', '*', '*', 6],
+          [7, '*', '*', 6, '*', 9, '*', '*', 3],
+          ['*', 8, '*', 4, '*', 2, '*', 6, '*'],
+          ['*', '*', '*', '*', 1, '*', '*', '*', '*'],
+          ['*', 7, '*', 3, '*', 8, '*', 2, '*'],
+          ['*', '*', '*', 7, '*', 1, '*', '*', '*'],
+          ['*', '*', '*', '*', '*', '*', '*', '*', '*']]
+
+    i2 = [['*', 7, 2, '*', '*', 3, 1, '*'],
+          ['*', '*', 8, '*', '*', '*', '*', 4],
+          [5, '*', '*', 4, '*', 7, '*', 9],
+          [8, '*', 1, '*', 5, '*', '*', '*'],
+          ['*', 1, '*', '*', '*', 5, '*', '*'],
+          ['*', '*', 7, '*', 8, '*', 6, 1],
+          [6, 3, '*', 2, '*', '*', '*', 5],
+          [1, '*', '*', '*', 3, '*', '*', '*'],
+          ['*', 4, '*', '*', 7, 8, '*', '*']]
+
+    i3 = [[6, '*', '*', '*', '*', '*', '*', '*'],
+          [5, 9, 4, 3, '*', '*', '*', '*'],
+          ['*', '*', '*', 7, '*', 4, '*', '*'],
+          ['*', '*', 3, '*', 1, '*', '*', '*'],
+          ['*', 5, '*', '*', '*', '*', 6, 2],
+          ['*', '*', 7, '*', '*', 9, 4, '*'],
+          ['*', 3, '*', '*', 6, '*', 9, '*'],
+          ['*', '*', '*', 2, 3, 1, '*', '*'],
+          ['*', '*', '*', 8, '*', '*', '*', '*']]
+
+    i4 = [['*', '*', '*', '*', '*', '*', '*', '*'],
+          [3, '*', '*', 2, 6, '*', '*', '*'],
+          ['*', '*', 3, '*', 7, 2, 8, '*'],
+          ['*', 7, '*', '*', 8, 1, '*', 5],
+          ['*', '*', '*', '*', '*', '*', '*', '*'],
+          ['*', 5, 1, '*', '*', 7, '*', 2],
+          ['*', 3, 6, '*', 2, 9, 1, '*'],
+          ['*', 9, '*', '*', '*', 4, '*', '*'],
+          ['*', '*', 8, '*', 5, '*', '*', '*']]
+
+    i5 = [['*', 3, '*', 5, 7, 6, 1, '*'],
+          ['*', 5, '*', '*', '*', '*', '*', '*'],
+          [7, '*', 9, '*', '*', 4, '*', '*'],
+          ['*', 8, '*', 6, '*', 7, '*', '*'],
+          [1, '*', 5, 3, '*', '*', '*', '*'],
+          [3, '*', '*', '*', '*', 1, 9, '*'],
+          [2, 9, 1, '*', 4, '*', 7, '*'],
+          [8, '*', '*', '*', 2, 3, '*', '*'],
+          ['*', '*', '*', '*', '*', '*', '*', '*']]
+
+    return [i1, i2, i3, i4, i5]
+
+
+def nivel_dificil():
+    d1 = [[6, '*', '*', 7, '*', '*', 9, '*'],
+          [7, 8, '*', '*', '*', '*', '*', '*'],
+          ['*', '*', 6, 8, '*', '*', '*', '*'],
+          [9, '*', '*', '*', '*', '*', 3, '*'],
+          ['*', 7, 5, '*', 2, 1, '*', '*'],
+          ['*', '*', '*', '*', '*', '*', '*', 6],
+          ['*', '*', '*', 1, 6, '*', 7, '*'],
+          ['*', '*', '*', '*', '*', 3, '*', 2],
+          ['*', '*', '*', 3, '*', '*', '*', 9]]
+
+    d2 = [['*', '*', 4, '*', 1, '*', '*', '*'],
+          ['*', '*', 2, '*', 7, '*', 8, '*'],
+          ['*', '*', '*', '*', '*', '*', 1, '*'],
+          ['*', 1, 5, '*', 8, 6, '*', '*'],
+          ['*', '*', '*', 2, '*', '*', '*', '*'],
+          ['*', '*', 1, '*', 6, '*', 5, '*'],
+          ['*', '*', 6, '*', 9, '*', 3, '*'],
+          ['*', 6, 8, '*', 3, 9, 2, '*'],
+          ['*', 9, '*', '*', '*', 8, '*', '*']]
+
+    d3 = [['*', '*', '*', '*', 8, '*', '*', '*'],
+          ['*', '*', '*', 3, '*', '*', 2, 1],
+          ['*', 5, 7, '*', 4, 9, '*', 3],
+          ['*', 8, '*', '*', '*', 3, 1, '*'],
+          ['*', '*', '*', '*', '*', '*', 4, 9],
+          [1, 3, '*', '*', '*', '*', '*', '*'],
+          ['*', 6, 9, '*', '*', '*', '*', '*'],
+          ['*', '*', 8, 2, '*', '*', '*', '*'],
+          ['*', 2, '*', 5, '*', '*', '*', 7]]
+
+    d4 = [['*', '*', '*', '*', '*', '*', '*', '*'],
+          [6, '*', '*', '*', '*', '*', 1, 8],
+          ['*', '*', 6, '*', 9, '*', '*', 4],
+          ['*', '*', 3, '*', '*', 8, 9, '*'],
+          [9, 2, '*', 1, '*', '*', 6, '*'],
+          ['*', '*', 2, '*', '*', 4, 7, '*'],
+          ['*', '*', 1, '*', 2, '*', '*', 9],
+          [7, '*', '*', '*', '*', '*', 8, 6],
+          ['*', '*', '*', '*', '*', '*', '*', '*']]
+
+    d5 = [[5, '*', '*', '*', '*', '*', '*', '*'],
+          ['*', '*', 6, '*', '*', '*', 2, '*'],
+          ['*', 8, '*', '*', 2, '*', '*', 9],
+          ['*', '*', '*', 3, '*', 9, '*', '*'],
+          ['*', 9, 2, 6, 1, 5, 8, '*'],
+          ['*', 7, '*', 5, '*', '*', '*', '*'],
+          [8, '*', 9, '*', '*', 2, 1, '*'],
+          ['*', '*', '*', '*', 3, '*', 6, '*'],
+          ['*', '*', '*', '*', '*', '*', '*', 8]]
+
+    return [d1, d2, d3, d4, d5]
+
+
+def solucion():
+    return [[9, 6, 3, 1, 7, 4, 2, 5, 8],
+            [1, 7, 8, 3, 2, 5, 6, 4, 9],
+            [2, 5, 4, 6, 8, 9, 7, 3, 1],
+            [8, 2, 1, 4, 3, 7, 5, 9, 6],
+            [4, 9, 6, 8, 5, 2, 3, 1, 7],
+            [7, 3, 5, 9, 6, 1, 8, 2, 4],
+            [5, 8, 9, 7, 1, 3, 4, 6, 2],
+            [3, 1, 7, 2, 4, 6, 9, 8, 5],
+            [6, 4, 2, 5, 9, 8, 1, 7, 3]]
+
+
+def VisualizaTabla_2(tabla, noModificables):
     i = 0
     j = 0
-    indices = [0,1,2,3,4,5,6,7,8]
+    indices = [0, 1, 2, 3, 4, 5, 6, 7, 8]
     fila_concat = '  |'
     fmto = ""
     for indice in indices:
@@ -53,10 +194,10 @@ def VisualizaTabla_2(tabla,noModificables):
 
     for fila in tabla:
         fila_concat = str(indices[i]) + " |"
-        fmto=""
+        fmto = ""
         j = 0
         for columna in fila:
-            coordenada = str(i) +","+ str(j)
+            coordenada = str(i) + "," + str(j)
 
             if coordenada in noModificables:
                 fmto = fmto + " " + Fore.WHITE + str(columna)
@@ -64,35 +205,37 @@ def VisualizaTabla_2(tabla,noModificables):
                 fmto = fmto + " " + Fore.BLUE + str(columna)
 
             if j == 2 or j == 5 or j == 8:
-                fmto = fmto +Fore.WHITE+"|"
+                fmto = fmto + Fore.WHITE + "|"
             j += 1
         print(fila_concat + fmto)
-        if i ==2 or i == 5 or i ==8:
+        if i == 2 or i == 5 or i == 8:
             print("   ---------------------")
-        i+=1
+        i += 1
+
 
 def obtenerNoModificables(tabla):
-        lista=[]
-        fila = 0
+    lista = []
+    fila = 0
 
-        for i in tabla:
-                col = 0
-                for j in i:
-                        if j != '*':
-                                coordenada = str(fila) +","+str(col)
-                                lista.append(coordenada)
-                        col += 1
-                fila +=1
-        return lista
+    for i in tabla:
+        col = 0
+        for j in i:
+            if j != '*':
+                coordenada = str(fila) + "," + str(col)
+                lista.append(coordenada)
+            col += 1
+        fila += 1
+    return lista
 
-def comprobacion_filas (tabla):
+
+def comprobacion_filas(tabla):
     bandera = True
 
     for filas in tabla:
 
         set_fila = set(filas)
         regla_1 = len(set_fila) == 9
-        regla_2 = max(set_fila) ==9
+        regla_2 = max(set_fila) == 9
         regla_3 = min(set_fila) == 1
 
         if (regla_1 and regla_2 and regla_3):
@@ -104,11 +247,12 @@ def comprobacion_filas (tabla):
 
     return bandera
 
-def comprobacion_columnas (tabla):
+
+def comprobacion_columnas(tabla):
     transpuesta = []
     # Se transpone la tabla para reutilizar la funcion de comprobacion_filas
     for i in range(len(tabla)):
-        new_fila =[]
+        new_fila = []
         for j in range(len(tabla[0])):
             new_fila.append(tabla[j][i])
         transpuesta.append(new_fila)
@@ -116,9 +260,10 @@ def comprobacion_columnas (tabla):
     bandera = comprobacion_filas(transpuesta)
     return bandera
 
-def comprobacion_cuadrante (tabla):
- #Se transforma cada cuadrante en una fila de la matriz para reutilizar la
- #
+
+def comprobacion_cuadrante(tabla):
+    # Se transforma cada cuadrante en una fila de la matriz para reutilizar la
+    #
     cuadrantes = []
     cuadrante_1 = []
     cuadrante_2 = []
@@ -130,28 +275,28 @@ def comprobacion_cuadrante (tabla):
     cuadrante_8 = []
     cuadrante_9 = []
 
-    for i in range (len(tabla)):
+    for i in range(len(tabla)):
         for j in range(len(tabla[0])):
-            if i<3:
-                if  j < 3:
+            if i < 3:
+                if j < 3:
                     cuadrante_1.append(tabla[i][j])
                 elif j > 2 and j < 6:
                     cuadrante_2.append(tabla[i][j])
-                elif j > 5 and j<9:
+                elif j > 5 and j < 9:
                     cuadrante_3.append(tabla[i][j])
-            elif i>2 and i<6:
+            elif i > 2 and i < 6:
                 if j < 3:
                     cuadrante_4.append(tabla[i][j])
                 elif j > 2 and j < 6:
                     cuadrante_5.append(tabla[i][j])
                 elif j > 5 and j < 9:
                     cuadrante_6.append(tabla[i][j])
-            elif i>5 and i<9:
-                if  j < 3:
+            elif i > 5 and i < 9:
+                if j < 3:
                     cuadrante_7.append(tabla[i][j])
                 elif j > 2 and j < 6:
                     cuadrante_8.append(tabla[i][j])
-                elif j > 5 and j<9:
+                elif j > 5 and j < 9:
                     cuadrante_9.append(tabla[i][j])
 
     cuadrantes.append(cuadrante_1)
@@ -166,17 +311,22 @@ def comprobacion_cuadrante (tabla):
 
     return comprobacion_filas(cuadrantes)
 
-def comprobar_tabla (tabla):
+
+def comprobar_tabla(tabla):
     boolean_filas = comprobacion_filas(tabla)
     boolean_columnas = comprobacion_columnas(tabla)
     boolean_cuadrante = comprobacion_cuadrante(tabla)
 
     return (boolean_filas and boolean_columnas and boolean_cuadrante)
 
-tabla = solucion()
-noModificables = obtenerNoModificables(crear_tabla_vacia())
-VisualizaTabla_2(tabla, noModificables)
+def comprobar_llenado_tabla(tabla):
+    bandera = False
 
-print(comprobar_tabla(tabla))
+    for i in tabla:
+        for j in i:
+            if j == '*':
+                bandera = True
+                return bandera
+                break;
 
-
+    return bandera
--- Main.py
+++ Main.py
@@ -1,12 +1,26 @@
+import Funciones as fun
+import random
+from IPython import display
+from colorama import Cursor, init, Fore
+
+init()
+
 #Mensaje de bienvenida
 
+print(Fore.BLUE+"-----------Sudoku-----------")
+print(Fore.GREEN + "Instrucciones:")
+print(Fore.GREEN+ "En la parte superior y lateral izquierda del sudoku")
+print(Fore.GREEN+ "aparecerán números con los que podrás ingresar la coordenada")
+print(Fore.GREEN+ "del número que deseas modificar.")
+print(Fore.GREEN+ "Recuerda: Solo puedes modificar las casillas en "+Fore.BLUE+"azul")
+print(Fore.WHITE+ "En caso de que quieras salir del juego sólo escribe " + Fore.RED+""'Salir'""+Fore.RESET)
+print("")
 print("Bienvenido, escoge un nivel para comenzar:")
-
 print("1. Fácil")
 print("2. Intermedio")
 print("3. Díficil")
 
-
+#Elección del nivel (Interacción con usuario)
 bandera = True
 while (bandera):
     nivel = input()
@@ -14,17 +28,68 @@ while (bandera):
     if nivel == "1":
         #   Elegir un sudoku de la lista Fácil
         print("Fácil")
+        sudokus = fun.nivel_facil()
+        tabla = random.choice(sudokus)
         bandera = False
 
     elif nivel == "2":
         #   Elegir un sudoku de la lista Intermedio
         print("intermedio")
+        sudokus = fun.nivel_inter()
+        tabla = random.choice(sudokus)
         bandera = False
 
     elif nivel == "3":
         #   Elegir un sudoku de la lista Dificil
         print("Díficil")
+        sudokus = fun.nivel_dificil()
+        tabla = random.choice(sudokus)
         bandera = False
 
     else:
         print("Hubo un error con tu opción, vuelve a intentarlo :D")
+
+#Se obtienen las coordenadas no modificables
+noModificables = fun.obtenerNoModificables(tabla)
+# Se muestra la tabla al usuario
+
+fun.VisualizaTabla_2(tabla,noModificables)
+print(Fore.WHITE + "¡Que empiece el juego!")
+
+while (fun.comprobar_llenado_tabla(tabla)):
+    print("Ingresa las coordenadas del dato:")
+    fila = input("Fila:")
+    columna = input("Columna:")
+
+    if(fila.lower() == 'salir' or columna.lower()=='salir'):
+        print("Hasta pronto!")
+        break;
+
+    coordenada = fila +","+ columna
+
+    #Comprobar si es una celda modificable
+    #Si es modificable, insertar el dato en la tabla
+    #De lo contrario, pedir de nuevo una coordenada
+
+    fun.VisualizaTabla_2(tabla,noModificables)
+
+#Si sale del while quiere decir que toda la tabla ya está llena
+
+print("Has terminado de llenar el sudoku, ¿Es tu respuesta final?")
+print("1.Si")
+print("2.No")
+
+
+
+casi_final = True
+while(casi_final):
+    respuesta = input()
+    if respuesta == "1":
+        solucion = fun.comprobar_tabla(tabla)
+        casi_final = False
+    elif respuesta == "2":
+        #Ciclo para modificar tabla
+        casi_final = False
+    else:
+        print("No te he entendido, vuelve a intentar")
+
